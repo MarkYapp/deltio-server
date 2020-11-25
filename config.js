50999182139c6231c174abd4236fdb625f61941e
Mark Yapp <markwyapp@gmail.com>
2020-11-24 20:34:17 -0500

    updated config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.DATABASE_URL =
-  process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/deltio-data';
+  process.env.MONGODB_URI || global.MONGODB_URI || 'mongodb://localhost/deltio-data';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/deltio-data-test';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
